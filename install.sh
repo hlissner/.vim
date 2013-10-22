8b58950dc405ad8a8363645066c171d22472afe3
Henrik Lissner <henrik@lissner.net>
2013-10-22 12:06:21 -0400

    Fix git detection & clean up

--- install.sh
+++ install.sh
@@ -2,26 +2,25 @@
 
 echo "Let's install vim!"
 
-if [ command -v git >/dev/null 2>&1 ]; then
-    if [ ! -d ~/.vim ]; then
-        echo "Cloning vim dotfiles..."
-        git clone https://github.com/hlissner/vim ~/.vim
-    fi
-
-    if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-        echo "Cloning neobundle..."
-        git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-    fi
-
-    echo "Writing symlinks..."
-    ln -sf ~/.vim/vimrc ~/.vimrc
-    ln -sf ~/.vim/gvimrc ~/.gvimrc
-    ln -sf ~/.vim/vimrc.bundles ~/.vimrc.bundles
-    ln -sf ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
-
-    echo "Done! Don't forget to run :NeoBundleInstall"
-    exit 0
-else
+if ! command -v git 2>&1 >/dev/null; then
     echo "Madness! Git isn't installed!"
     exit 1
 fi
+
+if [ ! -d ~/.vim ]; then
+    echo "Cloning vim dotfiles..."
+    git clone https://github.com/hlissner/vim ~/.vim
+fi
+
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+    echo "Cloning neobundle..."
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
+echo "Writing symlinks..."
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/gvimrc ~/.gvimrc
+ln -sf ~/.vim/vimrc.bundles ~/.vimrc.bundles
+ln -sf ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+
+echo "Done! Don't forget to run :NeoBundleInstall and compile YCM"
