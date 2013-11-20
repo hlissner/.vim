7d19c2a210d64dee9163bcfb3f570a4f531e315f
Henrik Lissner <henrik@lissner.net>
2013-11-20 15:36:17 -0500

    Add neobundle install script to vimrc, fix install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,6 @@ if [ ! -d ~/.vim ]; then
     git clone https://github.com/hlissner/vim ~/.vim
 fi
 
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-    echo "Cloning neobundle..."
-    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-fi
-
 echo "Writing symlinks..."
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
