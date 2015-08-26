fa23e770ff07528764667d65d02babc73831a95b
Henrik Lissner <henrik@lissner.net>
2015-08-26 18:59:19 -0400

    Add neobundle clone to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+[ ! -d ~/.vim/bundle/neobundle.vim ] && git clone --recursive https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
